368363ef0ed57e59e417e2e1fdb442e42e94fbbd
M.Inzamam Malik <malikasinger1@gmail.com>
2022-01-29 00:49:37 +0500

    done

--- bin/index.js
+++ bin/index.js
@@ -3,6 +3,8 @@
 const colors = require('colors');
 const greet = require("../lib/greet");
 
+console.log("this file runs when executed as globally installed packege");
+
 // print random greeting
 console.log(
     // wraps text with rainbow color formatting
